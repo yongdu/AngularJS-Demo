af3cccd913e161a5936652e58a30a8b86f45c309
Yong Du <Yong Du>
2016-10-28 17:41:57 +0200

    Second demo

--- js/script.js
+++ js/script.js
@@ -4,6 +4,15 @@ var myapp = angular.module("myModule",[]);
 // create the controller and registering the module all done in one line
 myapp.controller("myController", function($scope)
 	{
+		var employee = {
+
+        gender: 'Male'
+		};
+
+		$scope.employee = employee;
+		
 		$scope.message ="AngularJs Tutorial";
+		
+		
 	}
 );
